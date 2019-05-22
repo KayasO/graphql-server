9959cde4504f4da46c10349ddf58b54a95821330
Okan Kayas <okan-kayas@web.de>
2019-05-22 22:04:06 +0200

    Added prisma as dependency, added prisma to context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
 let links = [
   {
@@ -34,5 +35,6 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
